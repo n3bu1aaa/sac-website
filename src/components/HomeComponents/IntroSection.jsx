5191df17c8be834f4d4d1a7f67e594a114faf15c
Long <long@Longs-MacBook-Pro.local>
2025-04-30 00:48:29 -0400

    bg image

--- src/components/HomeComponents/IntroSection.jsx
+++ src/components/HomeComponents/IntroSection.jsx
@@ -1,9 +1,21 @@
+import bgImage from '../../assets/images/bg_image.jpg'
+
 const IntroSection = () => {
   return (
-    <section className="App-header min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white px-4 text-center">
+    <section 
+    style = {{ backgroundImage: `url(${bgImage})` }} 
+    className="App-header relative w-full min-h-screen flex flex-col justify-center items-center bg-gray-900 bg-cover bg-center text-white px-4 text-center">
+      <div className="absolute inset-0 bg-black bg-opacity-10"></div>
         <h1 className="text-5xl font-extrabold text-yellow-400 mb-4">
             Rubber Duck Gala
         </h1>
+        <p className="text-xl text-gray-300 mb-6">
+          Join us for a fun night of food, drinks and dancing!
+        </p>
+        <div className="text-lg text-gray-400 mb-8 space-y-2">
+          <p>📅 November 32, 2025 | 🕰️ 5pm–10pm</p>
+          <p>📍 Emily's Mansion Banquet Hall</p>
+        </div>
     </section>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,23 +5,23 @@ const NavBar = () => {
   const location = useLocation();
   return (
     <div>
-        <nav className="bg-gray-800 p-4 flex items-center justify-between">
+        <nav style = {{ backgroundColor: '#ebecee' }} className="p-4 flex items-center justify-between">
           <div className="flex items-center">
             <Link to="https://millikensac.com" className="flex items-center">
-                <FaHome className="h-8 w-8 text-white mr-3" />
+                <FaHome className="h-8 w-8 text-blue mr-3" />
             </Link>
           </div>
 
           {/* Links */}
-          <ul className="flex space-x-6 text-white text-lg">
+          <ul className="flex space-x-6 text-2xl">
             <li>
-              <Link to="https://millikensac.com" className="hover:underline">Home</Link>
+              <Link to="https://millikensac.com" className="hover:underline text-blue-800">Home</Link>
             </li>
             <li>
-              <Link to="/" className={`hover:underline ${location.pathname === '/' && 'underline text-yellow-400'}`}>Rubber Duck Gala</Link>
+              <Link to="/" className={`hover:underline ${location.pathname === '/' ? 'underline text-black-400' : 'text-blue-800'}`}>Rubber Duck Gala</Link>
             </li>
             <li>
-              <Link to="/ticketpage" className={`hover:underline ${location.pathname === '/ticketpage' && 'underline text-yellow-400'}`}>Tickets</Link>
+              <Link to="/ticketpage" className={`hover:underline ${location.pathname === '/ticketpage' ? 'underline text-black-400' : 'text-blue-800'}`}>Tickets</Link>
             </li>
           </ul>
         </nav>
