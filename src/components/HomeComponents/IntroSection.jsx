e011580e214b4409bbac6b66ee92ee7d01b54181
ethan <piancello0806@gmail.com>
2025-05-08 20:08:02 -0400

    done?

--- src/components/HomeComponents/IntroSection.jsx
+++ src/components/HomeComponents/IntroSection.jsx
@@ -1,37 +1,55 @@
-import bgImage from '../../assets/images/bg_image.jpg'
-import BlurText from '../Animations/BlurText'
-import GradientText from '../Animations/GradientText'
+import bgImage from "../../assets/images/bg_image.jpg";
+import BlurText from "../Animations/BlurText";
+import DecryptedText from "../Animations/DecryptedText";
 
 const IntroSection = () => {
   return (
-    <section 
-    style = {{ backgroundImage: `url(${bgImage})` }} 
-    className="App-header relative w-full min-h-screen flex flex-col justify-center items-center mb-16 bg-gray-900 bg-cover bg-center text-white px-4 text-center">
+    <section
+      style={{ backgroundImage: `url(${bgImage})` }}
+      className="App-header relative w-full min-h-screen flex flex-col justify-center items-center mb-16 bg-gray-900 bg-cover bg-center text-white px-4 text-center"
+    >
       <div className="w-full max-w-4xl p-8 bg-gray-800 rounded-lg shadow-lg bg-opacity-20 flex flex-col justify-center items-center">
         <h1 className="text-5xl font-extrabold text-black mb-4">
-        <BlurText
-          text="Rubber Duck Gala"
-          delay={500}
-          animateBy="words"
-          direction="top"
-          className='text-center'
-        />
+          <BlurText
+            text="Rubber Duck Gala"
+            delay={500}
+            animateBy="words"
+            direction="top"
+            className="text-center"
+          />
         </h1>
-        <GradientText
-          colors={["#26cc88", "#2f5ccc", "#26cc88", "#2f5ccc", "#26cc88"]}
-          animationSpeed={3}
-          showBorder={false}
-          className="text-xl text-black mb-6"
-        >
-          Join us for a fun night of food, drinks and dancing!
-        </GradientText>
+        <p className="text-xl text-black mb-6">
+          <DecryptedText
+            text="Join us for a fun night of food, drinks and dancing!"
+            animateOn="view"
+            revealDirection="start"
+            speed={30}
+            sequential={true}
+          />
+        </p>
         <div className="text-lg text-black mb-8 space-y-2">
-          <p>📅 November 32, 2025 | 🕰️ 5pm–10pm</p>
-          <p>📍 Emily's Mansion Banquet Hall</p>
+          <p>
+            <DecryptedText
+              text="📅 November 32, 2025 | 🕰️ 5pm–10pm"
+              animateOn="view"
+              revealDirection="start"
+              speed={50}
+              sequential={true}
+            />
+          </p>
+          <p>
+            <DecryptedText
+              text="📍 Emily's Mansion Banquet Hall"
+              animateOn="view"
+              revealDirection="start"
+              speed={50}
+              sequential={true}
+            />
+          </p>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default IntroSection
+export default IntroSection;
--- src/pages/TicketPage.js
+++ src/pages/TicketPage.js
@@ -1,6 +1,7 @@
 import Table from "../components/TicketComp/Table";
 import { useNavigate } from "react-router-dom";
 import bgImage from "../assets/images/bg_image2.jpg";
+import GradientText from '../components/Animations/GradientText';
 
 function TicketPage() {
     const navigate = useNavigate();
@@ -9,7 +10,14 @@ function TicketPage() {
         style = {{ backgroundImage: `url(${bgImage})`}}
         className = "App-header bg-center min-h-screen flex flex-col justify-center items-center bg-[#e3e3e3] px-4 text-center py-20">
             <div className="w-full max-w-4xl p-20 bg-gray-800 rounded-lg shadow-lg bg-opacity-10">
-                <h1 className = "font-extrabold text-black text-5xl mb-8">Buy Tickets!</h1>
+                <GradientText
+                    colors={["#26cc88", "#2f5ccc", "#26cc88", "#2f5ccc", "#26cc88"]}
+                    animationSpeed={3}
+                    showBorder={false}
+                    className="font-extrabold text-5xl mb-8"
+                >
+                    Buy Tickets!
+                </GradientText>
                 <Table />
                 <button
                     onClick={() => navigate("/ticketpage")}
