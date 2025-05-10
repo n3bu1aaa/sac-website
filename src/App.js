0112a8a564849eabad87643327fae419a1c6360c
ethan <piancello0806@gmail.com>
2025-05-09 20:00:07 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import TicketPage from './pages/TicketPage';
 import HomePage from './pages/HomePage';
